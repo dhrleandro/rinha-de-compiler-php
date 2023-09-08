e79a924ec49480ceb53757414c9679047fa8e371
Leandro Daher <117833992+dhrleandro@users.noreply.github.com>
2023-09-08 11:39:47 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+use LeandroDaher\RinhaDeCompilerPhp\Interpreter;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$interpreter = new Interpreter();
+var_dump($interpreter);
---
+++ src/Interpreter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace LeandroDaher\RinhaDeCompilerPhp;
+
+class Interpreter
+{
+    public function __construct() {
+        // constructor
+    }
+}
